a7207c3937bf84a3d32962fae2e6ae2174be5a8e
ABurroughs5 <aburroughs5@huskers.unl.edu>
2024-08-29 13:01:58 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Austin Burroughs/Antonio Jimenez
+ * Email: aburroughs5@huskers.unl.edu
+ * Date: 2024/08/29
+ *
+ * A simple program to print your name.
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Austin Burroughs\n");
+
+  return 0;
+}
